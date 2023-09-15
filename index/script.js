b86eddee0322ea85b4fef232babdb49130bce099
Ananazo <48922919+Ananazo@users.noreply.github.com>
2023-09-15 08:47:02 +0300

    javascript stuff for food iframe

--- index/script.js
+++ index/script.js
@@ -1,3 +1,8 @@
 function avaa(sivu){
     window.open(sivu)
-}
+};
+window.onload = function(){
+    var element = document.getElementById("myFrame");
+    element.style.width = "90vw";
+    element.style.height = "70vh";
+};
