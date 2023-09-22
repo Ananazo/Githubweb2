659e257d7d00e51611db8a488d64f16e07f51fae
Ananazo <48922919+Ananazo@users.noreply.github.com>
2023-09-22 10:23:57 +0300

    optimisaton

--- index/script.js
+++ index/script.js
@@ -6,8 +6,3 @@ window.onload = function(){
     element.style.width = "90vw";
     element.style.height = "70vh";
 };
-window.onload = function(){
-    var element = document.getElementById("myFrame");
-    element.style.width = "90vw";
-    element.style.height = "64vh";
-};
